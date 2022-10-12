7ba89a98c6598c52a7093b2392434ca5b4b1a639
Guilherme-FCM <guilherme_fcm@hotmail.com>
2022-10-11 21:28:14 -0300

    Create test suit valid logins

---
+++ Test Suites/Valid logins.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Valid logins</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0f72fe6d-aeec-41a8-b059-d39c0295a0a7</testSuiteGuid>
+   <testCaseLink>
+      <guid>73e7a7b8-f084-4292-a360-7574ef7b8f98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CT6 - Valid email and password</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>57dcb11e-5bac-4412-a21b-5bfc451d1b64</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Valid Login Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>57dcb11e-5bac-4412-a21b-5bfc451d1b64</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>c1baf1f3-6f2d-4ca6-bdef-85a7b012b5b3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>57dcb11e-5bac-4412-a21b-5bfc451d1b64</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>7f84dbfb-e408-440c-bd02-c6ba87779dd5</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
